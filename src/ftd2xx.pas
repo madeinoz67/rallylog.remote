6e9816589887d696ac04be186970718a27fade6c
Stephen Eaton <seaton@strobotics.com.au>
2013-03-24 01:12:42 +0800

    UPDATE added comment headers

--- src/ftd2xx.pas
+++ src/ftd2xx.pas
@@ -1,3 +1,29 @@
+{
+  Part of the RallyLog.Remote Project  http://code.google.com/p/rallylog
+
+  ----------------------------------------------------------
+
+  FTDI class implemtation of ftd2xx.dll for USB Serial Device
+
+  Not all functions of the DLL have been implemented
+
+  ----------------------------------------------------------
+
+  Copyright (C) 2013 Stephen Eaton seaton@strobotics.com.au
+
+  This source is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+  License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This code is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  A copy of the GNU General Public License is available on the World Wide Web at
+  <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing to the Free Software Foundation, Inc., 59
+  Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+}
+
 unit ftd2xx;
 
 {$mode objfpc}{$H+}
